4044ba010ef058c54c786168ab550289bd6b3da7
Ksendel <oksana.karachun14@gmail.com>
2021-01-16 17:48:02 +0200

    add slider block 7-10

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ $(function () {
 
     $('.banner-box__slider').slick({
         dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
         prevArrow: '<button class="banner-box__slider-btn banner-box__slider-btnprev"><img src="images/arrow_prev.png" alt=""></button>',
         nextArrow: '<button class="banner-box__slider-btn banner-box__slider-btnnext"><img src="images/arrow_next.png" alt=""></button>',
     });
@@ -18,3 +20,240 @@ $(function () {
         nextArrow: '<div class="rm-arr second"></div>'
     });
 });
+
+$(function () {
+    $('.new-items__body').slick({
+        infinite: false,
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        slidesToScroll: 1,
+
+
+        responsive: [
+            {
+                breakpoint: 2560,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    prevArrow: '<button class="n-img"><img src="images/arrow_prev.png" alt=""></button>',
+                    nextArrow: '<button class="n-img2""><img src="images/arrow_next.png" alt=""></button>',
+                }
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    prevArrow: '<button class="n-img"><img src="images/arrow_prev.png" alt=""></button>',
+                    nextArrow: '<button class="n-img2""><img src="images/arrow_next.png" alt=""></button>',
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    prevArrow: '<div class="new-button"><button class="news-arrow prev">❮</button></div>',
+                    nextArrow: '<button class="news-arrow next">❯</button>',
+
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    prevArrow: '<div class="new-button"><button class="news-arrow prev">❮</button></div>',
+                    nextArrow: '<button class="news-arrow next">❯</button>',
+                }
+            }
+        ],
+
+    });
+})
+
+$(function () {
+    $('.rec__body').slick({
+        infinite: false,
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        slidesToScroll: 1,
+
+
+        responsive: [
+            {
+                breakpoint: 2560,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    prevArrow: '<button class="n-img"><img src="images/arrow_prev.png" alt=""></button>',
+                    nextArrow: '<button class="n-img2""><img src="images/arrow_next.png" alt=""></button>',
+                }
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    prevArrow: '<button class="n-img"><img src="images/arrow_prev.png" alt=""></button>',
+                    nextArrow: '<button class="n-img2""><img src="images/arrow_next.png" alt=""></button>',
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    prevArrow: '<div class="new-button"><button class="news-arrow prev">❮</button></div>',
+                    nextArrow: '<button class="news-arrow next">❯</button>',
+
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    prevArrow: '<div class="new-button"><button class="news-arrow prev">❮</button></div>',
+                    nextArrow: '<button class="news-arrow next">❯</button>',
+                }
+            }
+        ],
+
+    });
+})
+
+$(function () {
+    $('.sale__body').slick({
+        infinite: false,
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        slidesToScroll: 1,
+
+
+        responsive: [
+            {
+                breakpoint: 2560,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    prevArrow: '<button class="n-img"><img src="images/arrow_prev.png" alt=""></button>',
+                    nextArrow: '<button class="n-img2""><img src="images/arrow_next.png" alt=""></button>',
+                }
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    prevArrow: '<button class="n-img"><img src="images/arrow_prev.png" alt=""></button>',
+                    nextArrow: '<button class="n-img2""><img src="images/arrow_next.png" alt=""></button>',
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    prevArrow: '<div class="new-button"><button class="news-arrow prev">❮</button></div>',
+                    nextArrow: '<button class="news-arrow next">❯</button>',
+
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    prevArrow: '<div class="new-button"><button class="news-arrow prev">❮</button></div>',
+                    nextArrow: '<button class="news-arrow next">❯</button>',
+                }
+            }
+        ],
+
+    });
+})
+
+
+$(function () {
+    $('.shares__body').slick({
+        infinite: false,
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        slidesToScroll: 1,
+
+
+        responsive: [
+            {
+                breakpoint: 2560,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    prevArrow: '<button class="n-img"><img src="images/arrow_prev.png" alt=""></button>',
+                    nextArrow: '<button class="n-img2""><img src="images/arrow_next.png" alt=""></button>',
+                }
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    prevArrow: '<button class="n-img"><img src="images/arrow_prev.png" alt=""></button>',
+                    nextArrow: '<button class="n-img2""><img src="images/arrow_next.png" alt=""></button>',
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    prevArrow: '<div class="new-button"><button class="news-arrow prev">❮</button></div>',
+                    nextArrow: '<button class="news-arrow next">❯</button>',
+
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    prevArrow: '<div class="new-button"><button class="news-arrow prev">❮</button></div>',
+                    nextArrow: '<button class="news-arrow next">❯</button>',
+                }
+            }
+        ],
+
+    });
+})
